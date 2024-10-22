77ff4c9d04c96a30cd11bf20da030cb02539bf4a
Mademidda Saseenthiran <mademidda.saseenthiran@hotmail.com>
2024-10-22 11:47:52 +0200

    Commandfactor was implemented

---
+++ C-CommandShell/Interfaces/ICommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_CommandShell.Interfaces
+{
+	internal interface ICommand
+	{
+		public List<String> parameters { get; set; }
+
+		protected void Execute();
+	}
+}
