66cbc61d264767ebd96ec76f5a2db4e5bbe1d749
anesganic <108254745+anesganic@users.noreply.github.com>
2024-10-22 11:15:25 +0200

    CommandParser.cs implementiert.

--- C-CommandShell/CommandParser.cs
+++ C-CommandShell/CommandParser.cs
@@ -1,12 +1,31 @@
-﻿using System;
+﻿using C_CommandShell;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace C_CommandShell
 {
     internal class CommandParser
     {
+        private readonly List<Type> commands;
+
+        public CommandParser()
+        {
+            commands = CommandFactory.GetCommands();
+        }
+
+        public Type GetCommand(string input)
+        {
+            string commandInUserInput = input.Split(' ').FirstOrDefault()?.ToLower() + "command";
+
+            return commands.FirstOrDefault(type => type.Name.Equals(commandInUserInput, StringComparison.OrdinalIgnoreCase));
+        }
+
+        public List<string> GetParameters(string input)
+        {
+            var parameters = input.Split(' ').Skip(1).ToList();
+
+            return parameters.Any() ? parameters : null;
+        }
     }
 }
