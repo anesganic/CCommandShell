df9ce52f9ac4cd0c7604150feb9598c4209ee40b
eliak <eliak@DESKTOP-ENR3JU5>
2024-10-24 13:32:02 +0200

    Content implementiert

---
+++ C-CommandShell/CommandContent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+using CCommandShell;
+
+namespace C_CommandShell
+{
+    internal class CommandContent
+    {
+        public List<string> Parameters { get; set; }
+        public String CurrentDir { get; set; }
+        public ShellEnvironment Environment { get; set; }
+
+        public CommandContent() 
+        { 
+            Parameters = new List<string>();
+        }
+    }
+}
