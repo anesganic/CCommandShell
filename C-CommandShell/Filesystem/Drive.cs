2e348e8da978614a7af0c4510eb22f0b565d8adf
anesganic <108254745+anesganic@users.noreply.github.com>
2024-10-29 11:28:19 +0100

    Drive.cs implementiert.

--- C-CommandShell/Filesystem/Drive.cs
+++ C-CommandShell/Filesystem/Drive.cs
@@ -4,9 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace C_CommandShell.Filesystem
+namespace CCommandShell.Filesystem
 {
-    internal class Drive
+    public class Drive
     {
+        public Directory RootDirectory { get; set; }
+        public string Name { get; set; }
+        public string Label { get; set; }
+        public long Size { get; set; }
+        public string FilesystemType { get; set; }
+        public string DriveType { get; set; }
+
+        public Drive(Directory rootDirectory, string name, string label, string fileSystemType, string driveType)
+        {
+            RootDirectory = rootDirectory;
+            Name = name;
+            Label = label;
+            FilesystemType = fileSystemType;
+            DriveType = driveType;
+        }
     }
 }
