f6d169a5e406210360e559187010181852d4d0d2
anesganic <108254745+anesganic@users.noreply.github.com>
2024-11-05 10:50:19 +0100

    VerCommand implementiert.

---
+++ C-CommandShell/Commands/VerCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using CCommandShell.Interfaces;
+
+
+namespace CCommandShell.Commands
+{
+    public class VerCommand : ICommand
+    {
+        public CommandContent CommandContent { get; set; } = new CommandContent();
+
+        //Constructor
+        public VerCommand()
+        {
+            CommandContent.OutputWriter = new CommandOutputWriter();
+        }
+        public VerCommand(ICommandOutputWriter commandOutputWriter)
+        {
+            CommandContent.OutputWriter = commandOutputWriter;
+        }
+
+
+        public void Execute()
+        {
+            OperatingSystem os = Environment.OSVersion;
+
+            CommandContent.OutputWriter.WriteLine("OS Version: " + os.Version.ToString());
+        }
+    }
+}
